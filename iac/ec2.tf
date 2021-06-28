762353ba94be9e5e0653dad67044b25b54478e58
auke-n <brs.morozov@gmail.com>
2021-06-28 16:26:12 +0300

    add resource build_server

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -33,20 +33,20 @@ resource "aws_instance" "web-server" {
 }
 
 
-//resource "aws_instance" "build-server" {
-//  ami                         = "ami-043097594a7df80ec"
-//  instance_type               = "t2.micro"
-//  subnet_id                   = aws_subnet.subnet_pbl1.id
-//  key_name                    = "lab"
-//  vpc_security_group_ids      = [aws_security_group.jenkins-ec2-sg.id, aws_security_group.build-sg.id]
-//  associate_public_ip_address = true
-//  iam_instance_profile        = aws_iam_instance_profile.ec2_ssm_profile.name
-//  user_data                   = file("config/user_data_build_server.sh")
-//
-//  tags = {
-//    Name = "build-server"
-//  }
-//  lifecycle {
-//    ignore_changes = [public_ip, public_dns, instance_state]
-//  }
-//}
+resource "aws_instance" "build-server" {
+  ami                         = "ami-043097594a7df80ec"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet_pbl1.id
+  key_name                    = "lab"
+  vpc_security_group_ids      = [aws_security_group.jenkins-ec2-sg.id, aws_security_group.build-sg.id]
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  user_data                   = file("config/user_data_build_server.sh")
+
+  tags = {
+    Name = "build-server"
+  }
+  lifecycle {
+    ignore_changes = [public_ip, public_dns, instance_state]
+  }
+}
