90d7024d448a347933e6d77ec343ca9fda396ee2
auke-n <brs.morozov@gmail.com>
2021-06-28 16:41:04 +0300

    modify user_data

--- iac/config/user_data_build_server.sh
+++ iac/config/user_data_build_server.sh
@@ -3,6 +3,40 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 sudo yum update -y
 
-# Java installation
+# Java and Git installation
 sudo amazon-linux-extras enable corretto8
-sudo yum install java-1.8.0-amazon-corretto-devel -y
+sudo yum install java-1.8.0-amazon-corretto-devel git -y
+
+#Create a SWAP-file
+sudo su
+dd if=/dev/zero of=/swapfile count=3072 bs=1MiB
+chmod 600 /swapfile
+mkswap /swapfile
+swapon  /swapfile
+echo "/swapfile   swap    swap    sw  0   0" >> /etc/fstab
+mount -a
+
+# Git installation
+yum install git -y
+
+# Jenkins installation
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins -y
+systemctl daemon-reload
+
+aws s3 cp s3://brso/jenkins.bk /var/lib/jenkins  --recursive
+chown jenkins:jenkins -R /var/lib/jenkins
+
+echo "jenkins ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+
+systemctl daemon-reload
+systemctl start jenkins
+systemctl enable jenkins
+
+# Docker installation
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+
+
